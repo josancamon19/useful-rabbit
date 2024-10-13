e519ca0627d0158f42c58dcee4b8e4510bf21305
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2024-10-13 15:57:38 -0700

    play audio when received segment

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart' show Level, Logger;
+import 'package:logger/logger.dart' show Level;
 import 'package:openai_realtime_dart/openai_realtime_dart.dart';
 
 class HomePage extends StatefulWidget {
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   final record = FlutterSoundRecorder(logLevel: Level.off);
   Queue<Uint8List> audioQueue = Queue<Uint8List>();
 
-  // final FlutterSoundPlayer _player = FlutterSoundPlayer(logLevel: Level.off);
+  final FlutterSoundPlayer _player = FlutterSoundPlayer(logLevel: Level.off);
 // Add audio bytes to the queue in order of arrival
 //   void enqueueAudioBytes(Uint8List pcmBytes) async{
 //     await _player.startPlayer(
@@ -65,10 +65,10 @@ class _HomePageState extends State<HomePage> {
   //   );
   // }
 
-  // _initiatePlayer() async {
-  //   await _player.openPlayer();
-  //   await _player.setVolume(1.0);
-  // }
+  _initiatePlayer() async {
+    await _player.openPlayer();
+    await _player.setVolume(1.0);
+  }
 
   _initRecording() async {
     // TODO: Check and request permission
@@ -99,7 +99,8 @@ class _HomePageState extends State<HomePage> {
 
   _initClient() async {
     client = RealtimeClient(
-      apiKey: '',
+      apiKey:
+          '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',
     );
 
     // Update session with instructions and transcription model
@@ -169,6 +170,7 @@ class _HomePageState extends State<HomePage> {
         },
       },
       (params) async {
+        print('get_weather params: $params');
         final lat = params['lat'];
         final lng = params['lng'];
         final location = params['location'];
@@ -246,12 +248,21 @@ class _HomePageState extends State<HomePage> {
       final delta = event['delta'];
       // final conversationItems = client.conversation.getItems();
       if (delta != null && delta['audio'] != null) {
-        // enqueueAudioBytes(delta['audio']);
+        // print(delta['audio']);
       }
       if (item['status'] == 'completed' &&
           item['formatted']['audio'] != null &&
           item['formatted']['audio'].length > 0) {
         print('Completed audio: ${item['content'][0]}');
+        Uint8List audio = item['formatted']['audio'];
+        await _player.startPlayer(
+          fromDataBuffer: audio,
+          codec: Codec.pcm16,
+          numChannels: 1,
+          sampleRate: 24000,
+        );
+        print(audio.runtimeType);
+        print(audio);
         setState(() {
           items.add(item);
         });
@@ -278,7 +289,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     _initClient();
     _initRecording();
-    // _initiatePlayer();
+    _initiatePlayer();
   }
 
   @override
@@ -297,29 +308,31 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Home Page'),
       ),
       body: Center(
-        // child: ListView.builder(
-        //   itemBuilder: (context, index) {
-        //     final item = items[index];
-        //     if (item == null) return SizedBox();
-        //     return ListTile(
-        //       title: Text(item['object']),
-        //       subtitle: Text(item['content'][0]['transcript'] ?? ''),
-        //     );
-        //   },
-        //   itemCount: items.length,
-        // ),
         child: ListView.builder(
           itemBuilder: (context, index) {
-            final item = realtimeEvents[index];
-            if (item['event']['audio'] != null) item['event']['audio'] = null;
-            if (item['event']['delta'] != null) item['event']['delta'] = null;
+            final item = items[index];
+            final transcript = item['content'][0]['transcript'] ?? '';
+            if (item == null || transcript.toString().isEmpty) return const SizedBox();
+            // print(item);
             return ListTile(
-              title: Text('${item['event']['type']} (${item['count'] ?? 1})'),
-              subtitle: Text(item['event'].toString()),
+              title: Text(item['object']),
+              subtitle: Text(transcript),
             );
           },
-          itemCount: realtimeEvents.length,
+          itemCount: items.length,
         ),
+        // child: ListView.builder(
+        //   itemBuilder: (context, index) {
+        //     final item = realtimeEvents[index];
+        //     if (item['event']['audio'] != null) item['event']['audio'] = null;
+        //     if (item['event']['delta'] != null) item['event']['delta'] = null;
+        //     return ListTile(
+        //       title: Text('${item['event']['type']} (${item['count'] ?? 1})'),
+        //       subtitle: Text(item['event'].toString()),
+        //     );
+        //   },
+        //   itemCount: realtimeEvents.length,
+        // ),
       ),
     );
   }
